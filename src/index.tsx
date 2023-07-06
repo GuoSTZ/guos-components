c01260c9a5e722823978a030c36e55c9fb7f34bd
guosheng <guosheng@mchz.com.cn>
2023-07-06 16:16:40 +0800

    feat: 删除无效代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Tabs, TabsProps } from 'antd';
 import LoadingView from './views/Loading';
 import './index.less';
