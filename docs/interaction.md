bbc87444b17302c72298277d132c509ad473a2cf
guosheng <guosheng@mchz.com.cn>
2023-07-10 16:00:07 +0800

    feat: 交互示例提交

---
+++ docs/interaction.md
@@ -0,0 +1,10 @@
+# 交互示例
+
+```tsx
+import React from 'react';
+import Interaction from 'base/pages/interaction';
+
+export default () => {
+  return <Interaction />;
+};
+```
---
+++ src/interaction/suspension/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.less';
+
+export default () => {
+  return (
+    <div id="suspension">
+      <div className="ball"></div>
+      <div className="platform"></div>
+    </div>
+  );
+};
