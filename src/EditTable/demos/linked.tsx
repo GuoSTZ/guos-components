992a6c198ae0c6213ff155bf2e3d69ef8be30371
guosheng <guosheng@mchz.com.cn>
2024-06-14 13:57:49 +0800

    feat: 增加EditTable demos

---
+++ src/EditTable/demos/linked.tsx
@@ -0,0 +1,78 @@
+import { Select } from 'antd';
+import { FormInstance } from 'antd/es/form/Form';
+import { EditTable } from 'guos-components';
+import React, { useRef } from 'react';
+
+const App = () => {
+  const tableRef = useRef();
+  const commonText = {
+    add: '新增',
+    edit: '编辑',
+    save: '保存',
+    remove: '删除',
+    removeConfirm: '确认删除吗？',
+    cancel: '取消',
+    cancelConfirm: '确认取消吗？',
+    editingMsg: '当前处于编辑状态，请先保存再做其他操作',
+    placeholderInput: '请输入',
+  };
+
+  const tableConfig = {
+    columns: [
+      {
+        title: '下拉框',
+        dataIndex: 'select',
+        width: 150,
+        editable: true,
+        editComponent: Select,
+        editComponentProps: {
+          placeholder: '请选择',
+          options: [
+            { label: '选项1', value: 1 },
+            { label: '选项2', value: 2 },
+          ],
+        },
+        editConfig: {
+          rules: [{ required: true, message: '请选择' }],
+        },
+      },
+      {
+        title: '输入框',
+        dataIndex: 'input',
+        editable: true,
+        editComponentProps: (form: FormInstance) => {
+          const select = form.getFieldValue('select');
+          if (select === 1) {
+            return {
+              placeholder: '选项1时，禁止输入',
+              disabled: true,
+            };
+          } else {
+            return {
+              placeholder: '请输入',
+            };
+          }
+        },
+        editConfig: (form: FormInstance) => {
+          const select = form.getFieldValue('select');
+          if (select === 1) {
+            return {
+              shouldUpdate: true,
+            };
+          } else {
+            return {
+              shouldUpdate: true,
+              rules: [{ required: true, message: '请输入' }],
+            };
+          }
+        },
+      },
+    ],
+    dataSource: [],
+    text: commonText,
+  };
+
+  return <EditTable {...tableConfig} ref={tableRef} />;
+};
+
+export default App;
---
+++ src/EditTable/index.md
@@ -0,0 +1,10 @@
+---
+toc: content
+---
+
+# EditTable
+
+## Demo
+
+<code src='./demos/base.tsx' title='基础使用' description='最简单的使用方式'></code>
+<code src='./demos/linked.tsx' title='联动' description='列之间存在联动关系，要实现联动关系，列配置中需要增加shouldUpdate属性'></code>
