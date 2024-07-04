a49d2090a021bcd33a7b2ebb1f5e2d74e0aa45de
guosheng <guosheng@mchz.com.cn>
2024-07-04 15:27:49 +0800

    feat: 移动类的动画处理

---
+++ src/experiment/animate/demos/move.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './move.less';
+
+const App = () => {
+  return (
+    <div id="card-wrap">
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  );
+};
+
+export default App;
--- src/experiment/index.md
+++ src/experiment/index.md
@@ -6,6 +6,7 @@ toc: content
 
 ## demo
 
+<code src="./animate/demos/move.tsx" title="动画效果 1"></code>
 <code src="./suspension/demos/base.tsx" title="悬浮"></code>
 <code src="./g6/demos/demo1.tsx" title="官方网格布局Demo"></code>
 <code src="./g6/demos/demo2.tsx" title="网格布局"></code>
