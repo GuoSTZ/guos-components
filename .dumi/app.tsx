100f3f35c5a67a551999edd9f43bc442780ea0ae
guosheng <guosheng@mchz.com.cn>
2024-07-09 14:58:55 +0800

    feat: 修改语句

--- .dumi/app.tsx
+++ .dumi/app.tsx
@@ -21,6 +21,7 @@ export function rootContainer(
           <div>本次提交内容: {'xxxx'}</div>
           <div>
             请<a onClick={() => window.location.reload()}>刷新页面</a>
+            以查看最新内容
           </div>
         </div>
       ),
