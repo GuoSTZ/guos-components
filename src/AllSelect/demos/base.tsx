e6719073c9c4ae34b93fbfb65f4c7cbdbed30404
guosheng <guosheng@mchz.com.cn>
2024-12-19 10:41:45 +0800

    feat: 组件调整

--- src/AllSelect/demos/base.tsx
+++ src/AllSelect/demos/base.tsx
@@ -3,25 +3,24 @@ import { AllSelect } from 'guos-components';
 import React from 'react';
 const App = () => {
   const [form] = Form.useForm();
+
   return (
     <Form form={form} labelCol={{ span: 4 }}>
       <Form.Item
         name="aaa"
         label="全选下拉框"
         rules={[{ required: true, message: '请选择aaa' }]}
+        // initialValue={[]}
       >
         <AllSelect
           style={{ width: 300 }}
           placeholder={'请选择'}
-          options={[
-            { label: '选项1a', value: 1, disabled: true },
-            { label: '选项2b', value: 2 },
-            { label: '选项3c', value: 3 },
-          ]}
+          options={[]}
           optionFilterProp="label"
           showSearch
-          selectAllValue={[1, 2, 3]}
-          transformSelectAllValue={(data) => data?.map((item) => item)}
+          // selectAllValue={['1', 2, 3]}
+          // transformSelectAllValue={func}
+          transformSelectAllValue
         />
       </Form.Item>
       <Form.Item wrapperCol={{ offset: 4 }}>
--- src/AllSelect/index.tsx
+++ src/AllSelect/index.tsx
@@ -3,7 +3,9 @@ import React, {
   useCallback,
   useEffect,
   useMemo,
+  useRef,
   useState,
+  memo,
 } from 'react';
 import { Select, SelectProps, Checkbox, Empty } from 'antd';
 import { CheckboxChangeEvent } from 'antd/es/checkbox';
@@ -25,13 +27,11 @@ export interface AllSelectProps extends SelectProps {
    */
   selectAllText?: string;
   /**
-   * 全选时的数据格式处理，为`true`时，自动处理成[value]格式，为`function`时，自定义处理格式
+   * 全选时的数据格式处理，为`true`时，自动处理成[value]格式，该属性开启后，组件仅第一次渲染时，如果value和可选值一致，那么会选中【全部】
    *
    * 该属性优先级高于 `selectAllValue` 属性
    */
-  transformSelectAllValue?:
-    | boolean
-    | ((data: Record<string, unknown>[]) => any);
+  transformSelectAllValue?: boolean;
 }
 
 type CompoundedComponent = ((props: AllSelectProps) => React.ReactElement) & {
@@ -39,6 +39,38 @@ type CompoundedComponent = ((props: AllSelectProps) => React.ReactElement) & {
   Option: typeof Select.Option;
   OptGroup: typeof Select.OptGroup;
 };
+
+/** 判断两个数组数据是否一致，如果是对象数组，那么只判定数据key值 */
+function areArraysEqual(
+  arr1: any[] = [],
+  arr2: any[] = [],
+  key: string = 'value',
+) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  const set1 = new Set();
+  arr1.forEach((item) => {
+    if (typeof item === 'object') {
+      set1.add(item?.[key]);
+    } else {
+      set1.add(item);
+    }
+  });
+
+  for (let i = 0; i < arr2.length; i++) {
+    const item = arr2[i];
+    if (typeof item === 'object' && !set1.has(item?.[key])) {
+      return false;
+    } else {
+      if (!set1.has(item)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 const AllSelect: CompoundedComponent = (props) => {
   const {
@@ -57,43 +89,59 @@ const AllSelect: CompoundedComponent = (props) => {
   const [lastSelect, setLastSelect] = useState(undefined as any);
   const [lastOption, setLastOption] = useState(undefined as any);
   const rowValue = fieldNames?.value || 'value';
+  const initRef = useRef(false);
+
+  // 可以选择的数据，去除disabled数据
+  const canSelectedOptions = useMemo(() => {
+    if (!options || options?.length === 0) {
+      return [];
+    }
+    const len = options?.length || 0;
+    const data = [];
+    for (let i = 0; i < len; i++) {
+      !options?.[i].disabled && data.push(options?.[i]?.[rowValue]);
+    }
+    return data;
+  }, [options, rowValue]);
 
   const handledSelectAllValue = useMemo(() => {
     if (!options || options?.length === 0) {
       return [];
     }
-    const len = options?.length;
-    const data = [];
-    for (let i = 0; i < len; i++) {
-      !options[i].disabled && data.push(options[i][rowValue]);
-    }
     if (
       typeof transformSelectAllValue === 'boolean' &&
       transformSelectAllValue
     ) {
-      return data;
-    } else if (typeof transformSelectAllValue === 'function') {
-      return transformSelectAllValue(options);
-    } else {
+      return canSelectedOptions;
+    }
+    // else if (typeof transformSelectAllValue === 'function') {
+    //   return transformSelectAllValue(options);
+    // }
+    else {
       return Array.isArray(selectAllValue) ? selectAllValue : [selectAllValue];
     }
-  }, [options, rowValue, transformSelectAllValue, selectAllValue]);
+  }, [transformSelectAllValue, selectAllValue, canSelectedOptions]);
 
   useEffect(() => {
-    if (
-      Array.isArray(value) &&
-      ((value.length === 1 && value[0] === handledSelectAllValue) || // 当传入字符串或者数字作为全选值时
-        value === handledSelectAllValue) // 当直接传入数组作为全选值时
+    console.log(value, handledSelectAllValue, '=====123');
+    if (Array.isArray(value) && value === handledSelectAllValue) {
+      setSelectedAll(true);
+    } else if (
+      handledSelectAllValue?.length > 0 &&
+      initRef.current === false &&
+      areArraysEqual(value, handledSelectAllValue, rowValue)
     ) {
       setSelectedAll(true);
     } else {
       setSelectedAll(false);
       setLastSelect(value);
     }
-  }, [value, handledSelectAllValue]);
+  }, [value, handledSelectAllValue, rowValue]);
 
   const selectOnChange = useCallback(
     (value: any, option: any) => {
+      // 数据变动后，立刻更改initRef
+      initRef.current = true;
       // 在输入框中，叉选【全部】时
       if (value?.length === 0 && selectedAll) {
         setSelectedAll(false);
@@ -117,20 +165,18 @@ const AllSelect: CompoundedComponent = (props) => {
 
   const selectAllOnChange = useCallback(
     (e: CheckboxChangeEvent) => {
+      // 数据变动后，立刻更改initRef
       const checked = e?.target?.checked;
       setSelectedAll(checked);
       if (checked) {
-        onChange?.(
-          handledSelectAllValue,
-          // @ts-ignore
-          [
-            {
-              key: selectAllText,
-              value: handledSelectAllValue,
-              label: selectAllText,
-            },
-          ],
-        );
+        onChange?.(handledSelectAllValue, [
+          {
+            key: selectAllText,
+            // @ts-ignore
+            value: handledSelectAllValue,
+            label: selectAllText,
+          },
+        ]);
       } else {
         onChange?.(lastSelect, lastOption);
       }
@@ -218,4 +264,4 @@ const AllSelect: CompoundedComponent = (props) => {
 };
 AllSelect.Option = Select.Option;
 AllSelect.OptGroup = Select.OptGroup;
-export default AllSelect;
+export default memo(AllSelect);
